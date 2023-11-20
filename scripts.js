c253319bf787714a9afa8f6ffe4f0bab3c8ced62
guilhermesouza23 <144975743+guilhermesouza23@users.noreply.github.com>
2023-11-20 18:38:47 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -34,6 +34,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Limpar o formulário após enviar
   form.reset();
+      document.addEventListener("DOMContentLoaded", function () {
+  const mainContent = document.querySelector(".content");
+
+  // ... Seções anteriores
+
+  document.getElementById("btnContato").addEventListener("click", function () {
+    scrollToSection("contato");
+  });
+
+  document.getElementById("submitBtn").addEventListener("click", function () {
+    const form = document.getElementById("contactForm");
+    const formData = new FormData(form);
+
+    // Aqui você pode enviar os dados para o seu servidor ou enviar por e-mail usando um serviço backend
+    // Por enquanto, vamos apenas exibir os dados no console para fins de demonstração
+    for (const pair of formData.entries()) {
+      console.log(pair[0] + ": " + pair[1]);
+    }
+
+    // Limpar o formulário após enviar
+    form.reset();
+  });
+});
+
 });
   }
 });
